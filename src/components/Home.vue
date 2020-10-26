46728d8294ada411f9f6543c843961386d399a90
mhusnim <mhusnimubarrok@gmail.com>
2020-10-26 16:03:00 +0700

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="hello">
     <section class="flex-ns vh-100 items-center">
-      /*<div class="mw7 ph5">
-        <img :src="imageUrl"> 
-      </div>*/
       <div class="tc tl-ns ph3">
         <h1 class="f3 f1-l fw2 mb3 mt4 mt0-ns">{{title}}</h1>
         <h2 class="f5 f3-l fw1 mb4 lh-title">{{subtitle}}</h2>
-        <a class="f3 link dim br1 ph4 pv3 mb2 dib white bg-dark-green" href="#" @click="showModal = true">Buy now with Bitcoin</a>
       </div>
     </section>
 
