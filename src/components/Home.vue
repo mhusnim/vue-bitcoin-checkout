9487ad86c9b89f32501918bdd1d099a0915a1b54
mhusnim <mhusnimubarrok@gmail.com>
2020-10-26 15:53:17 +0700

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="hello">
     <section class="flex-ns vh-100 items-center">
-      <div class="mw7 ph5">
-        <img :src="imageUrl"> 
-      </div>
+      //<div class="mw7 ph5">
+        //<img :src="imageUrl"> 
+      //</div>
       <div class="tc tl-ns ph3">
         <h1 class="f3 f1-l fw2 mb3 mt4 mt0-ns">{{title}}</h1>
         <h2 class="f5 f3-l fw1 mb4 lh-title">{{subtitle}}</h2>
